d73a0bd72a7cb5878c1b2a2e13227211acffa9a9
Hiroyuki Inoue <omega999kurahuto@yahoo.co.jp>
2015-03-12 21:24:41 +0900

    AnswerHistory 謎のエラー

--- app/Controller/AnswerHistoriesController.php
+++ app/Controller/AnswerHistoriesController.php
@@ -6,16 +6,35 @@ class AnswerHistoriesController extends ApiController {
     public $layout = null;
 
     public function add() {
-        if($this->request->is("post")){
-            $post_params["AnswerHistory"] = $this->request->data;
-            if($this->AnswerHistory->save($post_params)){
-                $post_params += array("code" => 200, "message" => "リクエストに成功しました。");
-                return $this->success($post_params);
-            }else{
-                return $this->validationError("AnswerHistory", $this->AnswerHistory->validationErrors);
-            }
+        $post_params["AnswerHistory"] = $this->request->data;
+        if($this->AnswerHistory->save($post_params)){
+            $post_params += array("code" => 201, "message" => "作成に成功しました。");
+            return $this->success($post_params);
         }else{
-            return $this->error("認証が失敗しているか、未認証の状態です。", 401);
+            return $this->validationError("AnswerHistory", $this->AnswerHistory->validationErrors);
+        }
+    }
+
+    public function index() {
+        //if(isset($this->request->query()){
+        //    return $this->success(array("message" => ));
+        //}
+        $this->AnswerHistory->set($this->request->query);
+        unset($this->AnswerHistory->validate["problem_id"]["notEmpty"]);
+        unset($this->AnswerHistory->validate["answer_flag"]["notEmpty"]);
+        if($this->AnswerHistory->validates()){
+            $findConditions = array(
+                                    "AnswerHistory.kentei_id" => $this->request->data["kentei_id"],
+                                    "AnswerHistory.user_id" => $this->request->data["user_id"]
+                                   );
+            if(isset($this->request->data["answer_flag"])){
+                $findConditions = array("AnswerHistory.answer_flag" => $this->request->data["answer_flag"]);
+            }
+            $findRespons = $this->AnswerHistory->find("all",array("conditions" => $findConditions));
+            $findRespons += array("code" => 200, "message" => "リクエストに成功しました。");
+            return $this->success($findRespons);
+        }else{
+            return $this->validationError("AnswerHistory", $this->AnswerHistory->validationErrors);
         }
     }
 }
