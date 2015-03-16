d3ff5a006a393775657ca27cba5b2a72c7b6d7e2
Hiroyuki Inoue <omega999kurahuto@yahoo.co.jp>
2015-03-17 01:24:22 +0900

    評価コメント追加,取得API ver1.0 作成完了

---
+++ app/Controller/EvaluateCommentsController.php
@@ -0,0 +1,48 @@
+<?php
+App::uses("ApiController","Controller");
+class EvaluateCommentsController extends ApiController {
+    public $name = "EvaluateComments";
+    public $uses = array("EvaluateComment");
+    public $layout = null;
+
+    public function add() {
+        $postParams["EvaluateComment"] = $this->request->data;
+        if($this->EvaluateComment->save($postParams)){
+            $postParams += array("code" => 201, "message" => "作成に成功しました。");
+            return $this->success($postParams);
+        }
+        return $this->validationError("EvaluateComment",$this->EvaluateComment->validationErrors);
+    }
+
+    public function index() {
+        $this->EvaluateComment->set($this->request->query);
+        $this->deleteValidFields($this->request->query);
+        if($this->EvaluateComment->validates()){
+            $conditions = $this->createConditions($this->request->query);
+            $comments = $this->EvaluateComment->find("all",array("conditions" => $conditions));
+            $comments += array("code" => 200, "message" => "リクエストに成功しました。");;
+            return $this->success($comments);
+        }
+        return $this->validationError("EvaluateComment", $this->EvaluateComment->validationErrors);
+    }
+
+    public function deleteValidFields($querys) {
+        //APIの仕様に準ずる(APIドキュメント参照)
+        unset($this->EvaluateComment->validate["evaluate_item_id"]["notEmpty"]);
+        unset($this->EvaluateComment->validate["evaluate_comment"]["notEmpty"]);
+        if(isset($querys["problem_id"])){
+            unset($this->EvaluateComment->validate["user_id"]["notEmpty"]["required"]);
+        }
+        if(isset($querys["user_id"])){
+            unset($this->EvaluateComment->validate["problem_id"]["notEmpty"]["required"]);
+        }
+    }
+
+    public function createConditions($querys) {
+        $conditions = array();
+        foreach($querys as $key => $query){
+            $conditions[] = array("EvaluateComment.".$key => $query);
+        }
+        return $conditions;
+    }
+}
