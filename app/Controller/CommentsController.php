354b9dbca34cff9472c2791d2fe9e36453577497
Hiroyuki Inoue <omega999kurahuto@yahoo.co.jp>
2015-03-22 01:52:13 +0900

    コメント取得API ver1.0 作成 完了

--- app/Controller/CommentsController.php
+++ app/Controller/CommentsController.php
@@ -13,4 +13,30 @@ class CommentsController extends ApiController {
         }
         return $this->validationError("Comment",$this->Comment->validationErrors);
     }
+
+    public function index() {
+        $this->Comment->set($this->request->query);
+        $this->deleteValidItem($this->request->query);
+        if($this->Comment->validates()){
+            $conditions = $this->createConditions($this->request->query);
+            $findComments = $this->Comment->find("all",array("conditions" => $conditions));
+            return $this->success($findComments);
+        }
+        return $this->validationError("Comment",$this->Comment->validationErrors);
+    }
+
+    public function deleteValidItem($querys) {
+        unset($this->Comment->validate["comment"]);
+        if(!isset($querys["from_user_id"])){
+            unset($this->Comment->validate["from_user_id"]);
+        }
+    }
+
+    public function createConditions($querys) {
+        $conditions = array();
+        foreach($querys as $key => $query){
+            $conditions += array("Comment.".$key => $query);
+        }
+        return $conditions;
+    }
 }
