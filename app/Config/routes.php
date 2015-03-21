5594a19da30d1087dcbea672801148db06b16017
Hiroyuki Inoue <omega999kurahuto@yahoo.co.jp>
2015-03-21 19:35:13 +0900

    タグ追加API ver1.0 作成 完了

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -21,7 +21,7 @@
  */
 
 // Setting Mapresources and Extension for API
-Router::mapResources(array('users', 'logins','AnswerHistories', 'Problems'));
+Router::mapResources(array('users', 'logins','AnswerHistories', 'Problems', 'Tags'));
 Router::parseExtensions('json');
 
 /**
