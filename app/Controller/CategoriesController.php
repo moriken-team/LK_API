4226d8dbae1668bfa9069876ae868524a7432307
Hiroyuki Inoue <omega999kurahuto@yahoo.co.jp>
2015-03-21 17:33:11 +0900

    カテゴリー取得API ver1.0 作成 完了

--- app/Controller/CategoriesController.php
+++ app/Controller/CategoriesController.php
@@ -13,4 +13,28 @@ class CategoriesController extends ApiController {
         }
         return $this->validationError("Category",$this->Category->validationErrors);
     }
+
+    public function index() {
+        $this->Category->set($this->request->query);
+        unset($this->Category->validate["name"]["notEmpty"]);
+        if($this->Category->validates()){
+            $conditions = $this->createConditions($this->request->query);
+            $findCategories = $this->Category->find("all",array("conditions" => $conditions));
+            $categories["Categories"] = array();
+            foreach($findCategories as $category){
+                $categories["Categories"][] = $category;
+            }
+            $categories += array("code" => 200, "message" => "リクエストに成功しました。");
+            return $this->success($categories);
+        }
+        return $this->validationError("Category",$this->Category->validationErrors);
+    }
+
+    public function createConditions($querys) {
+        $conditions = array();
+        foreach($querys as $key => $query){
+            $conditions[] = array("Category.".$key => $query);
+        }
+        return $conditions;
+    }
 }
