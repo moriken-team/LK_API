0edb792e8ab00d996218249d9ee46bf01b1c91a3
Hiroyuki Inoue <omega999kurahuto@yahoo.co.jp>
2015-06-19 01:09:55 +0900

    add スポット情報を登録するためのAPIを作成

--- app/Controller/SpotsController.php
+++ app/Controller/SpotsController.php
@@ -5,4 +5,13 @@ class SpotsController extends ApiController {
     public $uses = array("Spot");
     public $layout = null;
 
+    public function add() {
+        $postSpot = $this->request->data;
+        if($this->Spot->save($postSpot)){
+            $response["Spot"] = $postSpot;
+            $response += array("code" => 201, "message" => "作成に成功しました。");
+            return $this->success($response);
+        }
+        return $this->validationError("Spot",$this->Spot->validationErrors);
+    }
 }
