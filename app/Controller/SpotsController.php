b1429266e093fd3bffefd566e5b9bb57eb684fa4
Hiroyuki Inoue <omega999kurahuto@yahoo.co.jp>
2015-06-19 02:29:13 +0900

    add スポット情報を取得するためのAPIを作成

--- app/Controller/SpotsController.php
+++ app/Controller/SpotsController.php
@@ -14,4 +14,27 @@ class SpotsController extends ApiController {
         }
         return $this->validationError("Spot",$this->Spot->validationErrors);
     }
+
+    public function index() {
+        $this->Spot->set($this->request->query);
+        if($this->Spot->validates(array("fieldList" => array("kentei_id")))) {
+            $conditions = $this->createConditions($this->request->query);
+            $findSpots = $this->Spot->find("all", array("conditions" => $conditions));
+            $response["Spots"] = array();
+            foreach($findSpots as $number => $spot) {
+                $response["Spots"][$number] = $spot;
+            }
+            $response += array("code" => 200, "message" => "リクエストに成功しました。");
+            return $this->success($response);
+        }
+        return $this->validationError("Spot", $this->validationErrors);
+    }
+
+    public function createConditions($querys) {
+        $conditions = array();
+        foreach($querys as $column => $param) {
+            $conditions[] = array("Spot.".$column => $param);
+        }
+        return $conditions;
+    }
 }
